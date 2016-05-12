6fd3d62ac360b3b889a56ba15386582cd68c11a2
Jayson Buquia <jayson.buquia@codeandtheory.com>
2016-05-12 22:42:20 +0800

    Remove Nova MVC for admin

--- admin/app/views/admin/form.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-use core\view;
-
-View::rendertemplate('_header');
-View::rendertemplate('form');
-View::rendertemplate('_footer');
--- admin/app/views/admin/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-use core\view;
-
-View::rendertemplate('_header');
-View::rendertemplate('index');
-View::rendertemplate('_footer');
