cc33a3abaca8da75a3c7654dd932699a0ecedd50
Jayson Buquia <jayson.buquia@codeandtheory.com>
2016-05-17 23:22:33 +0800

    Header link fix

--- views/admin/_header.php
+++ views/admin/_header.php
@@ -19,7 +19,7 @@
 		<div class="row">
 			<nav class="red">
 			    <div class="nav-wrapper red">
-			      <div class="col"><a href="/admin" class="brand-logo">Admin Panel</a></div>
+			      <div class="col"><a href="<?= app_path('/admin') ?>" class="brand-logo">Admin Panel</a></div>
 			      <ul class="right">
 			        <li><a href="<?= app_path('admin/add') ?>"><i class="material-icons">add</i></a></li>
 			      </ul>
